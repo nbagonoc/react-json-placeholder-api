d711b484a906421c93c0bb5fffc0916cc08125db
Niccu Bagonoc <nbagonoc@Niccus-MacBook-Air.local>
2018-07-29 20:26:25 +0800

    just playing around with REACT, News API, with Fetch, and AXIOS Method.

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+import News from "./News/News";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <News />
+      </div>
+    );
+  }
+}
+
+export default App;
